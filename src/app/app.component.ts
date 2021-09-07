2a710d89ecdb0c7ee65475f29b9786cbd9527af0
ilkay_duzgun <ilkay_duzgun@hotmail.com>
2021-09-07 11:22:52 +0300

    mas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BilimselAnketler';
+  title = 'Bilimsel Anketler';
+  name='Anket'
 }
