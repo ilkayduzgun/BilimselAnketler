fee75fdf8ddaabae4df0d01891434c025167dcce
ilkay_duzgun <ilkay_duzgun@hotmail.com>
2021-09-13 21:41:55 +0300

    master2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { Screen1Component } from './screen1/screen1.component';
 import { Screen2Component } from './screen2/screen2.component';
 import { Screen3Component } from './screen3/screen3.component';
 import { Screen4Component } from './screen4/screen4.component';
+import { Screen5Component } from './screen5/screen5.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Screen6Component } from './screen6/screen6.component';
 
 
 
@@ -21,7 +24,10 @@ import { Screen4Component } from './screen4/screen4.component';
     Screen1Component,
     Screen2Component,
     Screen3Component,
-    Screen4Component
+    Screen4Component,
+    Screen5Component,
+    CarouselComponent,
+    Screen6Component
   ],
   imports: [
    [NgbPaginationModule, NgbAlertModule],
---
+++ src/app/screen5/screen5.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'; 
+
+@Component({
+  selector: 'app-screen5',
+  templateUrl: './screen5.component.html',
+  styleUrls: ['./screen5.component.css']
+})
+export class Screen5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
